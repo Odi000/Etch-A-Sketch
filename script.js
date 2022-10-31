e7f8fee525b880d4c356480517d62b975b517c50
Odi000 <mandimandi.ro@gmail.com>
2022-10-31 22:34:23 +0100

    All buttons are functional

--- script.js
+++ script.js
@@ -5,34 +5,41 @@ const pickedColor = () => document.getElementById('clr-picker').value;
 const btnFunctions = {
     color: (e) => {
         e.target.style.backgroundColor = pickedColor();
-        screen.classList.add('cursorio')
     },
     shader: (e) => {
         const regExp = /[\d]+/g;
         let bgColor = e.target.style.backgroundColor;
         let shade = 25;
-
+        
         if(bgColor === '') {
             bgColor = e.target.style.backgroundColor = 'rgb(255,255,255)'
         }
-
+        
         const rgbValues = bgColor.match(regExp);
-
+        
         e.target.style.backgroundColor = (
             `rgb(${rgbValues[0]-shade},${rgbValues[1]-shade},${rgbValues[2]-shade})`
             );
-    },
-    eraser: (e) => {
-        e.target.style.backgroundColor = '';
-    },
-    reset: (e) => {
-        createAppendDivs();
+        },
+        eraser: (e) => {
+            e.target.style.backgroundColor = '';
+        },
+        reset: (e) => {
+            createAppendDivs();
     },
     warm: (e) => {
-        
+        const red = getColorValue(128,255);
+        const green = getColorValue(0,255);
+        const blue = getColorValue(0,50);
+
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
     },
     cold: (e) => {
-        
+        const red = getColorValue(0,50);
+        const green = getColorValue(0,255);
+        const blue = getColorValue(128,255);
+
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
     }
 }
 
@@ -51,27 +58,31 @@ function createAppendDivs(side = 16) {
     }
 }
 
-function parseButton(e){
+function parseButton(){
     if(this.id === 'reset') return btnFunctions[this.id]();
     if(this.classList.contains('pressed')){
-       return this.classList.remove('pressed'); 
+        return this.classList.remove('pressed'),
+        screen.classList.remove('hover');
     } 
-    buttons.forEach(button => button.classList.remove('pressed'));
+    buttons.forEach(button => {
+        button.classList.remove('pressed');
+        screen.classList.remove('hover');
+    })
     this.classList.add('pressed');
 }
 
 function checkPressedButton(e){
     buttons.forEach(button => {
-        if(button.classList.contains('pressed')) btnFunctions[button.id](e);
+        if(button.classList.contains('pressed')){
+            btnFunctions[button.id](e);
+            screen.classList.add('hover');
+        }
     });
 }
 
-// function drawColor(){}
-// function drawShade(){}
-// function erase(){}
-// function resetAll(){}
-// function drawWarm(){}
-// function drawCold(){}
+function getColorValue(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 
 
